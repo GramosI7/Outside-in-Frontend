3e6004df35b374e362561fcc0375a84738fb581b
Gramos <imeri.gramos@gmail.com>
2021-02-08 15:30:14 +0100

    Style app with material design

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import RestaurantScreen from './components/RestaurantScreen';
 import {Provider} from 'react-redux';
 import store from './store';
 
+import {createMuiTheme} from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import {ThemeProvider} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
+
 function App() {
   return (
     <Provider store={store}>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Opinion Ate</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <RestaurantScreen />
+        </Container>
+      </ThemeProvider>
     </Provider>
   );
 }
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,19 +2,23 @@ import {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {loadRestaurants} from '../store/restaurants/actions';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 export const RestaurantList = ({loadRestaurants, restaurants}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
   return (
-    <div>
-      <ul>
-        {restaurants.map(restaurant => (
-          <li key={restaurant.id}>{restaurant.name}</li>
-        ))}
-      </ul>
-    </div>
+    <List>
+      {restaurants.map(restaurant => (
+        <ListItem key={restaurant.id}>
+          <ListItemText>{restaurant.name}</ListItemText>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
